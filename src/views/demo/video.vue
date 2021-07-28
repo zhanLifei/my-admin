525cbc9afdabd24b9c79b060e921c70e75675f56
zhanLifei <zhanlifei123@126.com>
2021-07-28 10:57:27 +0800

    合并

--- src/views/demo/video.vue
+++ src/views/demo/video.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="app-container">
     <div><video-player :options="videoOption" class="video" /></div>
+    <quill-editor
+    :content="content"
+    :options="editorOption"
+    @change="onEditorChange($event)"
+  />
+  <div id="div1"></div>
   </div>
 </template>
 
 <script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+import { quillEditor } from 'vue-quill-editor'
+import E from 'wangeditor'
+const editor = new E('#div1')
+editor.create()
 require("videojs-flash");
 import VideoPlayer from "@/components/VideoPlayer.vue";
 export default {
   components: {
-    VideoPlayer
+    VideoPlayer,
+    quillEditor
   },
   data() {
     return {
@@ -19,6 +33,10 @@ export default {
   computed: {
     videoOption() {
       return {
+        content: '<h2>I am Example</h2>',
+        editorOption: {
+          // Some Quill options...
+        },
         autoplay: true, //自动播放
         controls: true, //显示控件
         width:400,
@@ -35,7 +53,10 @@ export default {
   mounted() {},
 
   methods: {
-   
+   onEditorChange({ quill, html, text }) {
+        console.log('editor change!', quill, html, text)
+        this.content = html
+      }
   }
 };
 </script>
